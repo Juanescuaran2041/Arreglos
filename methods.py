a02e39eab78ed5cd93fce0f370bcebcad7d5fd1c
Juanescuaran2041 <juanescuaransantander@gmail.com>
2026-08-13 18:45:39 -0500

    Feath: first commit

---
+++ methods.py
@@ -0,0 +1,63 @@
+"""
+Métodos para manipular Arrays 1D y 2D
+Los métodos deben ser completados por el estudiante
+"""
+
+
+# Metodos array 1D
+
+def get_second_element(array: list):
+    """Obtiene el segundo elemento del array 1D"""
+    pass
+
+
+def insert_value_1d(array: list, index: int, value):
+    """Inserta un valor en la posición especificada (1D)"""
+    pass
+
+
+def search_value_1d(array: list, value):
+    """Busca un valor en el array 1D y retorna la posición"""
+    pass
+
+
+def remove_value_1d(array: list, index: int):
+    """Elimina un elemento en la posición especificada (1D)"""
+    pass
+
+
+def sort_1d(array: list):
+    """Ordena el array 1D"""
+    pass
+
+
+# Metodos array 2D
+
+def get_element_2d(array: list, row: int, col: int):
+    """Obtiene el elemento en posición [row][col]"""
+    pass
+
+
+def insert_value_2d(array: list, row: int, col: int, value):
+    """Inserta un valor en la posición [row][col]"""
+    pass
+
+
+def remove_value_2d(array: list, row: int, col: int):
+    """Elimina un elemento en [row][col]"""
+    pass
+
+
+def search_value_2d(array: list, value):
+    """Busca un valor en el array 2D y retorna (row, col)"""
+    pass
+
+
+def sum_row_2d(array: list, row: int):
+    """Suma los elementos de una fila"""
+    pass
+
+
+def sum_col_2d(array: list, col: int):
+    """Suma los elementos de una columna"""
+    pass
